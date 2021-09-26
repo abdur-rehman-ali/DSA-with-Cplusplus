ce31c17169aef6845ac1a93be411976d174fa31a
abdur-rehman-ali <alijoyia911@gmail.com>
2021-09-26 18:06:48 +0500

    delete functionality implemented

--- 03_Linked list/01_Singly linked list/singlyLinkedLIst.cpp
+++ 03_Linked list/01_Singly linked list/singlyLinkedLIst.cpp
@@ -1,225 +1,278 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 struct node
 {
     int data;
     node *next;
-
 };
-
 
 class singlyLinkedList
 {
-    private:
-    node*head;
-    public:
+private:
+    node *head;
 
+public:
     singlyLinkedList()
     {
-        head=NULL;
+        head = NULL;
     }
 
-    void append(node* newNode);
-    void prepend(node* newNode);
-    void update(int data,int value);
+    bool valueExist(int value);
+    void append(node *newNode);
+    void prepend(node *newNode);
+    void update(int data, int value);
     void deleteValue(int value);
     void display();
 };
 
+//This function will check whether search value exist in list or not
+bool singlyLinkedList::valueExist(int value)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == value)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
 //This fucntion will add value at end of linked list
-void singlyLinkedList:: append(node* newNode)
+void singlyLinkedList::append(node *newNode)
 {
     //If linked list is empty then this condition will be true
-    if(head==NULL)
+    if (head == NULL)
     {
-        head=newNode;
-        cout<<"Value added at head of the linked list";
+        head = newNode;
+        cout << "Value added at head of the linked list" << endl;
+        ;
     }
     else
     {
         //Made temporary pointer for traversal of list
-        node *temp=head;
-        while(temp->next!=NULL)
+        node *temp = head;
+        while (temp->next != NULL)
         {
-            temp=temp->next;
+            temp = temp->next;
         }
-        temp->next=newNode;
-        cout<<"Value added by traversing the linked list";
+        temp->next = newNode;
+        cout << "Value added by traversing the linked list" << endl;
     }
 }
-
 
 //This fucntion will add value at start of linked list
-void singlyLinkedList::prepend(node* newNode)
+void singlyLinkedList::prepend(node *newNode)
 {
-    if(head==NULL)
+    if (head == NULL)
     {
-        head=newNode;
+        head = newNode;
     }
     else
     {
-        node *temp=head;
-        newNode->next=temp;
-        head=newNode;
-        cout<<"Value added at head"<<endl;
-
+        node *temp = head;
+        newNode->next = temp;
+        head = newNode;
+        cout << "Value added at head" << endl;
     }
 }
 
-
-//This fucntion will update the value 
-void singlyLinkedList::update(int data,int value)
+//This fucntion will update the value
+void singlyLinkedList::update(int data, int value)
 {
-    if(head==NULL)
+    if (head == NULL)
     {
-        cout <<"Linked list is empty"<<endl;
+        cout << "Linked list is empty" << endl;
     }
     else
     {
-        node *temp=head;
-        while(temp!=NULL)
+        if (valueExist(data))
         {
-            if (temp->data==data)
+
+            node *temp = head;
+            while (temp != NULL)
             {
-                temp->data=value;
-                cout<<"value updated"<<endl;
-                return;
+                if (temp->data == data)
+                {
+                    temp->data = value;
+                    cout << "value updated" << endl;
+                    return;
+                }
+                temp = temp->next;
             }
-            temp=temp->next;
+        }
+        else
+        {
+            cout << "Value does not exist" << endl;
         }
     }
-    cout<<"Value does not exist"<<endl;
 }
-
 
 //This function will delete the value
 void singlyLinkedList::deleteValue(int value)
 {
-    if (head==NULL)
+    if (head == NULL)
     {
-        cout<<"List is empty";
+        cout << "List is empty...." << endl;
     }
     else
     {
-        if(head->data==value)
+        if (valueExist(value))
         {
-            head=head->next;
-            cout<<"Head value deleted..."<<endl;
+            if (head->data == value)
+            {
+                head = head->next;
+                cout << "Head value deleted..." << endl;
+            }
+            else
+            {
+                node *prevPtr = head;
+                node *temp = head->next;
+                while (temp != NULL)
+                {
+                    if (temp->data == value)
+                    {
+                        prevPtr->next = temp->next;
+                        cout << "Value deleted..." << endl;
+                        break;
+                    }
+                    prevPtr = prevPtr->next;
+                    temp = temp->next;
+                }
+            }
         }
-
+        else
+        {
+            cout << "Value does not exist..." << endl;
+        }
     }
-
 }
 
-
 //This function will display all the values in the list
-void singlyLinkedList:: display()
+void singlyLinkedList::display()
 {
-    if (head==NULL)
+    if (head == NULL)
     {
-        cout <<"Linked list is empty";
+        cout << "Linked list is empty...." << endl;
     }
     else
     {
         //Made temporary pointer for traversal of list
-        node *temp=head;
-        while(temp!=NULL)
+        node *temp = head;
+        while (temp != NULL)
         {
-            cout<<" "<<temp->data<<"-->";
-            temp=temp->next;
+            cout << " " << temp->data << "-->";
+            temp = temp->next;
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-
-
-
 void menu()
 {
-    int option,value,data;
-   
+    int option, value, data;
+
     singlyLinkedList singlyLinkedListObj;
 
     do
     {
-    cout<<"0)Exit()"<<endl<<endl;
-    cout<<"1)Append()"<<endl<<endl;
-    cout<<"2)Prepend()"<<endl<<endl;
-    cout<<"3)Update()"<<endl<<endl;
-    cout<<"4)Delete()"<<endl<<endl;
-    cout<<"5)Display()"<<endl<<endl;
-    cout<<"6)Clear screen()"<<endl<<endl;
-    cin>>option;
-   
-        switch(option)
+        cout << "0)Exit()" << endl
+             << endl;
+        cout << "1)Append()" << endl
+             << endl;
+        cout << "2)Prepend()" << endl
+             << endl;
+        cout << "3)Update()" << endl
+             << endl;
+        cout << "4)Delete()" << endl
+             << endl;
+        cout << "5)Display()" << endl
+             << endl;
+        cout << "6)Clear screen()" << endl
+             << endl;
+        cin >> option;
+
+        switch (option)
         {
-            case 0:
-            {   
-                
-                break;
-            }
-            case 1:
-            {   
-                cout<<"Enter the value to append"<<endl;
-                cin>>value;
-                node* newNode = new node();
-                newNode->data=value;
-                newNode->next=NULL;
+        case 0:
+        {
 
-                singlyLinkedListObj.append(newNode);
-                break;
-            }
-            case 2:
-            {
-                cout<<"Enter the value to prepend"<<endl;
-                cin>>value;
-                node* newNode = new node();
-                newNode->data=value;
-                newNode->next=NULL;
+            break;
+        }
+        case 1:
+        {
+            cout << "Enter the value to append" << endl;
+            cin >> value;
+            node *newNode = new node();
+            newNode->data = value;
+            newNode->next = NULL;
 
-                singlyLinkedListObj.prepend(newNode);
-                break;
-                
-                break;
-            }
-            case 3:
-            {
-                cout<<"Enter the which value you want to update"<<endl;
-                cin>>data;
-                cout<<"Enter the updated value "<<endl;
-                cin>>value;
-                singlyLinkedListObj.update(data,value);
-               
-                break;
-            }
-            case 4:
-            {
-                cout<<"Enter the which value you want to delete"<<endl;
-                cin>>value;
-                singlyLinkedListObj.deleteValue(value);
-
-                break;
-            }
-            case 5:
-            {
-                singlyLinkedListObj.display();
-                break;
-            }
-            case 6:
-            {
-                system("cls");
-                break;
-            }
-            default:
-            {
-                cout<<"Select correct option"<<endl<<endl;
-            }
+            singlyLinkedListObj.append(newNode);
             
+            system("pause");
+            system("cls");
+            break;
+        }
+        case 2:
+        {
+            cout << "Enter the value to prepend" << endl;
+            cin >> value;
+            node *newNode = new node();
+            newNode->data = value;
+            newNode->next = NULL;
+
+
+            singlyLinkedListObj.prepend(newNode);
+            system("pause");
+            system("cls");
+            break;
+        }
+        case 3:
+        {
+            cout << "Enter the which value you want to update" << endl;
+            cin >> data;
+            cout << "Enter the updated value " << endl;
+            cin >> value;
+            singlyLinkedListObj.update(data, value);
+
+            system("pause");
+            system("cls");
+            break;
+        }
+        case 4:
+        {
+            cout << "Enter the which value you want to delete" << endl;
+            cin >> value;
+            singlyLinkedListObj.deleteValue(value);
+
+            system("pause");
+            system("cls");
+            break;
+        }
+        case 5:
+        {
+            singlyLinkedListObj.display();
+
+            system("pause");
+            system("cls");
+            break;
+        }
+        case 6:
+        {
+            system("cls");
+            break;
+        }
+        default:
+        {
+            cout << "Select correct option" << endl
+                 << endl;
+        }
         }
 
-}while(option!=0);
+    } while (option != 0);
 }
 
 int main()
